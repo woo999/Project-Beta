4ecb3cb2692955629d12f3ccb42c3d0cfea77641
woo999 <69070706+woo999@users.noreply.github.com>
2026-07-29 00:25:51 +0800

    Export strategy signal prototypes

--- src/beta_backtest/__init__.py
+++ src/beta_backtest/__init__.py
@@ -2,5 +2,12 @@
 
 from .costs import round_trip_cost
 from .point_in_time import BacktestGuardError, validate_execution
+from .signals import breakout_pullback_holds, momentum_return
 
-__all__ = ["BacktestGuardError", "round_trip_cost", "validate_execution"]
+__all__ = [
+    "BacktestGuardError",
+    "breakout_pullback_holds",
+    "momentum_return",
+    "round_trip_cost",
+    "validate_execution",
+]
